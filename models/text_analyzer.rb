6046c2337d59d78f497d9bf971f18ad4055c5fbe
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-22 06:59:35 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -20,24 +20,27 @@ class TextAnalyzer
   end
  
   def most_used_letter
-    s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
+    s1 = @text.downcase.gsub(/[^a-z]/, '') #gets rid of spaces
     arr = s1.split('')
     arr1 = arr.uniq
     arr2 = {}
- 
-    arr1.map do |c|
-      arr2[c] =  arr.count(c)
+    arr1.map do |c| 
+     arr2[c] =  arr.count(c)
     end
- 
-    biggest = { arr2.keys.first => arr2.values.first }
- 
+    biggest = { 
+      :most_used_letter  => arr2.keys.first,
+      :letter_count => arr2.values.first 
+    }
     arr2.each do |key, value|
-      if value > biggest.values.first
-        biggest = {}
-        biggest[key] = value
+      if value > biggest.values[1]
+        biggest = {
+          :most_used_letter => "",
+          :letter_count => ""
+        }
+        biggest[:most_used_letter] = key
+        biggest[:letter_count] = value
       end
     end
- 
     biggest
   end
 end
